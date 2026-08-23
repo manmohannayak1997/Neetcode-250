df36b9316f446365b7ef1779f1e5af7fd81c27aa
Man Mohan Nayak <99341350+manmohannayak1997@users.noreply.github.com>
2026-08-23 16:08:09 +0530

    Time: 3 ms (33.87%), Space: 11.3 MB (18.21%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,27 +1,58 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+
+//         vector<string> words;
+
+//         string word;
+
+//         for (char c : s) {
+
+//             if (c != ' ') {
+//                 word += c;
+//             }
+//             else {
+
+//                 if (!word.empty()) {
+//                     words.push_back(word);
+//                     word.clear();
+//                 }
+//             }
+//         }
+
+//         // Last word
+//         if (!word.empty()) {
+//             words.push_back(word);
+//         }
+
+//         reverse(words.begin(), words.end());
+
+//         string ans;
+
+//         for (int i = 0; i < words.size(); i++) {
+
+//             if (i > 0)
+//                 ans += " ";
+
+//             ans += words[i];
+//         }
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     string reverseWords(string s) {
+
+        stringstream ss(s);
 
         vector<string> words;
 
         string word;
 
-        for (char c : s) {
-
-            if (c != ' ') {
-                word += c;
-            }
-            else {
-
-                if (!word.empty()) {
-                    words.push_back(word);
-                    word.clear();
-                }
-            }
-        }
-
-        // Last word
-        if (!word.empty()) {
+        while (ss >> word) {
             words.push_back(word);
         }
 
